35b0da4c1d5f72d50ef55b411adcfc54506350f0
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-12 16:30:41 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -30,9 +30,6 @@ def update_status
   end
   
 
-
-
-
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   epic_tragedy
 end
